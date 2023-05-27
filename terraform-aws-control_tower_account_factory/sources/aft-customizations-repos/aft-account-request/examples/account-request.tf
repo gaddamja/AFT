70bd018d4c3c804f61cd3dec045432fa24c8eb10
nimisha434 <nimisha.shah434@gmail.com>
2023-05-27 17:18:29 +0100

    Create account

--- terraform-aws-control_tower_account_factory/sources/aft-customizations-repos/aft-account-request/examples/account-request.tf
+++ terraform-aws-control_tower_account_factory/sources/aft-customizations-repos/aft-account-request/examples/account-request.tf
@@ -1,41 +1,41 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-module "sandbox_account_01" {
+module "DEV_account" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "john.doe@amazon.com"
-    AccountName  = "sandbox-account-01"
+    AccountEmail = "nimisha@tradeandcloud.com"
+    AccountName  = "DEV_account"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Sandbox"
+    ManagedOrganizationalUnit = "Trade_AFT"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "john.doe@amazon.com"
-    SSOUserFirstName = "John"
-    SSOUserLastName  = "Doe"
+    SSOUserEmail     = "nimisha@tradeandcloud.com"
+    SSOUserFirstName = "nimisha"
+    SSOUserLastName  = "shah"
   }
 
   account_tags = {
-    "ABC:Owner"       = "john.doe@amazon.com"
+    "ABC:Owner"       = "nimisha@tradeandcloud.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
     "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "102"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
+    "ABC:DivCode"     = "100"
+    "ABC:BUCode"      = "TradeBU"
+    "ABC:Project"     = "TradeandCloud"
   }
 
-  change_management_parameters = {
-    change_requested_by = "John Doe"
-    change_reason       = "testing the account vending process"
-  }
+  #change_management_parameters = {
+   # change_requested_by = "John Doe"
+   # change_reason       = "testing the account vending process"
+  #}
 
-  custom_fields = {
-    custom1 = "a"
-    custom2 = "b"
-  }
+  #custom_fields = {
+   # custom1 = "a"
+   # custom2 = "b"
+  #}
 
-  account_customizations_name = "sandbox-customizations"
+  #account_customizations_name = "sandbox-customizations"
 }
